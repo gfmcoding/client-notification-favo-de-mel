73b915610b73d9f2062a783a2d78808ba1615453
Guilherme S <gfmcoding@gmail.com>
2020-08-17 00:30:34 -0400

    implementing the client

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using NATS.Client;
+
+namespace nats_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var connection = new ConnectionFactory().CreateConnection();
+        
+
+            var orderTabNotificationSub = connection.SubscribeAsync("order_tab_notification", (sender, args ) => {
+                Console.WriteLine($"Sender: Favo de mel Application");
+                Console.WriteLine($"Nofitification: {args.Message.Subject}");
+                Console.WriteLine($"Message:{Encoding.UTF8.GetString(args.Message.Data)}");
+                Console.WriteLine();
+            });
+
+            orderTabNotificationSub.Start();
+
+            var orderNotificationSub = connection.SubscribeAsync("order_notification", (sender, args ) => {
+                Console.WriteLine($"Sender: Favo de mel Application");
+                Console.WriteLine($"Nofitification: {args.Message.Subject}");
+                Console.WriteLine($"Message:{Encoding.UTF8.GetString(args.Message.Data)}");
+                Console.WriteLine();
+            });
+
+            orderNotificationSub.Start();
+
+            Console.WriteLine("Starting the listen to notifications");
+            Console.WriteLine("To stop, press the key c");
+            Console.WriteLine();
+
+            var pressed = Console.ReadKey();
+            while (pressed.Key == ConsoleKey.C)
+            {
+                Console.WriteLine("Pressed the key c, is the command to stop.");    
+                break;
+            }
+
+            Console.WriteLine("Stopping of listen to notifications");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# client-notification-favo-de-mel
+# Cliente de notificações favo de mel
+Cliente para ouvir e visualizar as notificações ocorridas das operações da favo de mel api.
+
+
+## Executando
+
+Após clonar o projeto e em seu ambiente os serviços nasts já estarem levantados, execute na raiz do projeto:
+> dotnet run
+
+Para encerrar aperte c.
